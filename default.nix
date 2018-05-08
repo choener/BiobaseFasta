cb2ffe2b6381a39d07af409bdc0f91cc50a6869c
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-05-08 16:55:55 +0200

    disabled checks for pipes-group

--- default.nix
+++ default.nix
@@ -1,13 +1,18 @@
 with import <nixpkgs> {};
 let
-  sourceOverrides = {
+  pipes-group-override = haskellPackages.override {
+    overrides = self: super: {
+      # old doctest
+      pipes-group = haskell.lib.dontCheck super.pipes-group;
+    };
+  };
+  sourceOverrides = pipes-group-override.extend (haskell.lib.packageSourceOverrides {
     BiobaseTypes = ../Lib-BiobaseTypes;
     DPutils = ../Lib-DPutils;
     ForestStructures = ../Lib-ForestStructures;
     PrimitiveArray = ../Lib-PrimitiveArray;
     SciBaseTypes = ../Lib-SciBaseTypes;
-  };
-  pipes-group-no-test = pipes-group.extend (self : super : {});
+  });
 in
-haskellPackages.extend (haskell.lib.packageSourceOverrides sourceOverrides)
+sourceOverrides
 
