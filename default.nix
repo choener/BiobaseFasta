861ff68c9add47e3b1e72b7f95c12ffde53daa67
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-12 15:20:08 +0200

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,39 @@
-with import <nixpkgs> {};
-let
-  packageOverrides = haskellPackages.override {
-    overrides = self: super: {
-      # old doctest
-      pipes-group = haskell.lib.dontCheck super.pipes-group;
-    };
-  };
-  sourceOverrides = packageOverrides.extend (haskell.lib.packageSourceOverrides {
+with (import <nixpkgs> {});
+with haskell.lib;
+
+rec {
+  ##currently not needed?
+  #packageOverrides = haskellPackages.override {
+  #  overrides = self: super: {
+  #    # old doctest
+  #    pipes-group = haskell.lib.dontCheck super.pipes-group;
+  #  };
+  #};
+  hsPkgs = haskellPackages.extend (packageSourceOverrides {
+    bimaps = ../Lib-bimaps;
     BiobaseFasta = ./.;
+    #BiobaseBlast = ./.;
+    #BiobaseENA = ../Lib-BiobaseENA;
     BiobaseTypes = ../Lib-BiobaseTypes;
+    #BiobaseXNA = ../Lib-BiobaseXNA;
     DPutils = ../Lib-DPutils;
     ForestStructures = ../Lib-ForestStructures;
+    OrderedBits = ../Lib-OrderedBits;
     PrimitiveArray = ../Lib-PrimitiveArray;
     SciBaseTypes = ../Lib-SciBaseTypes;
   });
-in
-sourceOverrides
-
+  hsShell = with hsPkgs; shellFor {
+    packages = p: [ p.BiobaseFasta ];
+    withHoogle = true;
+    buildInputs = [
+      cabal-install ghc
+      bimaps
+      BiobaseTypes
+      DPutils
+      ForestStructures
+      OrderedBits
+      PrimitiveArray
+      SciBaseTypes
+    ];
+  };
+}
