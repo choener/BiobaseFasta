6a1e3ac7c5c54d161e46e9d1c8a71200d3b716b0
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:43 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,22 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, base, BiobaseTypes, bytestring, filepath, lens
+, QuickCheck, resourcet, stdenv, streaming, streaming-bytestring
+, tasty, tasty-golden, tasty-hunit, tasty-quickcheck, tasty-silver
+, tasty-th, text
+}:
+mkDerivation {
+  pname = "BiobaseFasta";
+  version = "0.2.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base BiobaseTypes bytestring lens resourcet streaming
+    streaming-bytestring
+  ];
+  testHaskellDepends = [
+    base bytestring filepath QuickCheck resourcet streaming
+    streaming-bytestring tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-silver tasty-th text
+  ];
+  homepage = "https://github.com/choener/BiobaseFasta";
+  description = "streaming FASTA parser";
+  license = stdenv.lib.licenses.gpl3;
+}
