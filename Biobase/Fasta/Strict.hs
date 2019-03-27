20e8aa2b0229329c902c703263d9e1056c3bc9a1
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-27 16:38:33 +0100

    enabled some unit tests again

--- Biobase/Fasta/Strict.hs
+++ Biobase/Fasta/Strict.hs
@@ -17,9 +17,15 @@ import           Control.Lens
 import           Data.Bifunctor (first)
 import           Data.ByteString (ByteString)
 import           Data.String.Conversions
+import           Data.Void
 import           GHC.Generics (Generic)
+import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Streaming as BSS
+import qualified Streaming.Prelude as SP
 
+import           Biobase.Fasta.Streaming as FS
 import           Biobase.Types.BioSequence
 
 
@@ -33,6 +39,10 @@ data Fasta which ty = Fasta
   deriving (Eq,Ord,Read,Show,Generic)
 makeLenses ''Fasta
 
+-- | If you don't want to deal with the phantom types.
+
+type FastaUntyped = Fasta Void Void
+
 -- | Render a 'Fasta' entry to a 'ByteString'. Will end with a final @\n@ in
 -- any case.
 
@@ -42,6 +52,17 @@ fastaToByteString k' Fasta{..} = BS.cons '>' (_header^._Wrapped) <> "\n" <> go (
   where go (BS.splitAt k → (hd,tl))
           | BS.null hd = mempty
           | otherwise  = hd <> "\n" <> go tl
+        k = max 1 k'
+
+-- | Render a 'Fasta' entry to a 'Builder'. Will end with a final @\n@ in
+-- any case.
+
+fastaToBuilder ∷ Int → Fasta which ty → BB.Builder
+{-# Inlinable fastaToBuilder #-}
+fastaToBuilder k' Fasta{..} = BB.char8 '>' <> (BB.byteString $ _header^._Wrapped) <> BB.char8 '\n' <> go (_fasta^._Wrapped)
+  where go (BS.splitAt k → (hd,tl))
+          | BS.null hd = mempty
+          | otherwise  = BB.byteString hd <> BB.char8 '\n' <> go tl
         k = max 1 k'
 
 -- | Try to parse a 'ByteString' as a 'Fasta', failing with 'Left', succees
@@ -54,6 +75,15 @@ byteStringToFasta (BS.lines → ls)
   | Just (z, hdr) ← BS.uncons h, z `BS.elem` ">;" = Right $ Fasta { _header = SequenceIdentifier hdr, _fasta = BioSequence $ BS.concat ts }
   | otherwise = Left "no '>'/';' first character"
   where h:ts = ls
+
+-- | Try to parse a 'ByteString' as multiple 'Fasta' entries. Even though this
+-- is using the underlying streaming interface, this is not streaming.
+
+byteStringToMultiFasta
+  ∷ BSL.ByteString → [Fasta which ty]
+{-# Inlinable byteStringToMultiFasta #-}
+byteStringToMultiFasta bsl = map (view windowedFasta) $ runIdentity bss
+  where bss = SP.toList_ . streamingFasta (HeaderSize maxBound) (OverlapSize 0) (CurrentSize maxBound) $ BSS.fromLazy bsl
 
 -- | A lens that goes from a 'BioSequenceWindow' to a 'Fasta'.
 
--- tests/properties.hs
+++ tests/properties.hs
@@ -1,70 +1,69 @@
 
 module Main where
 
-import           Prelude as P
-import           Data.Functor.Of
-import qualified Data.ByteString.Char8 as BS
+import           Control.Lens (view)
+import           Control.Monad.Trans.Resource (runResourceT, ResourceT(..), MonadResource)
 import           Data.ByteString.Streaming as BSS
 import           Data.ByteString.Streaming.Char8 as S8
 import           Data.ByteString.Streaming.Internal (ByteString(..))
-import           Streaming as S
-import           Streaming.Prelude as SP
-import           Test.Tasty
-import           Test.Tasty.HUnit
-import           Test.Tasty.QuickCheck as QC
---import           Test.Tasty.Silver as S
---import           Test.Tasty.Silver.Interactive as SI
-import           Test.Tasty.TH
-import           Control.Monad.Trans.Resource (runResourceT, ResourceT(..), MonadResource)
+import           Data.Functor.Of
+import           Data.Void
+import           Prelude as P
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BL8
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Test.Tasty.Golden as Golden
+import           Streaming as S
+import           Streaming.Prelude as SP
 import           System.FilePath (takeBaseName, replaceExtension)
-import qualified Data.ByteString.Lazy.Char8 as BL8
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck as QC
+import           Test.Tasty.TH
+
+import           Biobase.Types.BioSequence
+import           Biobase.Types.Strand
 
 import           Biobase.Fasta.Streaming
+import           Biobase.Fasta.Strict
 
 
 
-main :: IO ()
-main = return ()
-
-{-
-
--- * golden tests
-
-eachFasta (Header h) (Overlap o) (Current c p) = SP.yield (h,o,c)
-
-readFastaFile ∷ FilePath → IO [(BS.ByteString,BS.ByteString,BS.ByteString)]
-readFastaFile f = do
-  let s = 1000000
-  xs :> r ← runResourceT
-          $ SP.toList
-          $ streamingFasta (HeaderSize s) (OverlapSize 0) (CurrentSize s) eachFasta
-          $ S8.readFile f
-  return xs
-
-lazyFastaOut = BL8.concat . P.map go
-  where go (h,o,c) = BL8.concat
-          [ BL8.fromStrict h
-          , BL8.pack "\n"
-          , BL8.fromStrict o
-          , BL8.pack "\n"
-          , BL8.fromStrict c
-          , BL8.pack "\n"
-          ]
-
-goldenTests ∷ IO TestTree
-goldenTests = do
-  fastaFiles ← Golden.findByExtension [".fa"] "./tests/"
-  return $ testGroup "readFastaFile golden tests"
-    [ Golden.goldenVsString
-        (takeBaseName fastaFile) -- test name
-        goldenFile -- golden file path
-        (lazyFastaOut <$> readFastaFile fastaFile) -- action whose result is tested
-    | fastaFile <- fastaFiles
-    , let goldenFile = replaceExtension fastaFile ".fa-golden"
-    ]
+-- -- * golden tests
+-- 
+-- eachFasta (HeaderSize h) (OverlapSize o) (CurrentSize c p) = SP.yield (h,o,c)
+-- 
+-- readFastaFile ∷ FilePath → IO [(BS.ByteString,BS.ByteString,BS.ByteString)]
+-- readFastaFile f = do
+--   let s = 1000000
+--   xs :> r ← runResourceT
+--           $ SP.toList
+--           $ streamingFasta (HeaderSize s) (OverlapSize 0) (CurrentSize s) eachFasta
+--           $ S8.readFile f
+--   return xs
+-- 
+-- lazyFastaOut = BL8.concat . P.map go
+--   where go (h,o,c) = BL8.concat
+--           [ BL8.fromStrict h
+--           , BL8.pack "\n"
+--           , BL8.fromStrict o
+--           , BL8.pack "\n"
+--           , BL8.fromStrict c
+--           , BL8.pack "\n"
+--           ]
+-- 
+-- goldenTests ∷ IO TestTree
+-- goldenTests = do
+--   fastaFiles ← Golden.findByExtension [".fa"] "./tests/"
+--   return $ testGroup "readFastaFile golden tests"
+--     [ Golden.goldenVsString
+--         (takeBaseName fastaFile) -- test name
+--         goldenFile -- golden file path
+--         (lazyFastaOut <$> readFastaFile fastaFile) -- action whose result is tested
+--     | fastaFile <- fastaFiles
+--     , let goldenFile = replaceExtension fastaFile ".fa-golden"
+--     ]
 
 -- * unit tests
 
@@ -77,30 +76,34 @@ smallInlineFasta = P.unlines
   , "890"
   ]
 
-smallTest ∷ Int → Int → Int → Of [(BS.ByteString,BS.ByteString,BS.ByteString)] ()
+smallTest ∷ Int → Int → Int → Of [BioSequenceWindow Void Void 1] ()
 smallTest h o c = runIdentity
          . toList
-         . streamingFasta (HeaderSize h) (OverlapSize o) (CurrentSize c) go
+--         . SP.map (view windowedFasta)
+         . streamingFasta (HeaderSize h) (OverlapSize o) (CurrentSize c)
          . S8.fromStrict
          $ BS.pack smallInlineFasta
-  where go (Header h) (Overlap o) (Current c _) = yield (h,o,c)
+  where go (HeaderSize h) (OverlapSize o) (CurrentSize c) = yield (h,o,c)
 
 smallTest333 = testCase "3/3/3" $ do
   let res :> r = smallTest 3 3 3
   assertEqual "return is null" () r
   assertEqual "length is 4" 4 (P.length res)
-  assertEqual "!!0" (">Aa","","123") (res!!0)
-  assertEqual "!!1" (">Bb","","456") (res!!1)
-  assertEqual "!!2" (">Bb","456","7") (res!!2)
-  assertEqual "!!3" (">Cc","","890") (res!!3)
+  assertEqual "!!0" (BioSequenceWindow "Aaa" ""    "123" "" PlusStrand 1) (res!!0)
+  assertEqual "!!1" (BioSequenceWindow "Bbb" ""    "456" "" PlusStrand 1) (res!!1)
+  assertEqual "!!2" (BioSequenceWindow "Bbb" "456" "7"   "" PlusStrand 4) (res!!2)
+  assertEqual "!!3" (BioSequenceWindow "Ccc" ""    "890" "" PlusStrand 1) (res!!3)
+  --
+  assertEqual "!!0/Fasta" (Fasta "Aaa" "123") (view windowedFasta $ res!!0)
+  assertEqual "!!1/Fasta" (Fasta "Bbb" "456") (view windowedFasta $ res!!1)
+  assertEqual "!!2/Fasta" (Fasta "Bbb" "7"  ) (view windowedFasta $ res!!2)
+  assertEqual "!!3/Fasta" (Fasta "Ccc" "890") (view windowedFasta $ res!!3)
 
 main :: IO ()
 main = do
-  gs ← goldenTests
-  defaultMain $ testGroup "all tests"
-    [ testGroup "Golden" [gs]
-    , testGroup "unit tests" [smallTest333]
-    ]
-
--}
+--   gs ← goldenTests
+   defaultMain $ testGroup "all tests"
+--     [ testGroup "Golden" [gs]
+     [ testGroup "unit tests" [smallTest333]
+     ]
 
